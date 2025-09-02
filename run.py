84fc9a149d738e67584c7d4b5c20ef17c0ff685a
taurus5650 <taurus_5650@hotmail.com>
2025-09-02 23:24:10 +0800

    Feat: basic

---
+++ run.py
@@ -0,0 +1,11 @@
+import streamlit
+
+streamlit.set_page_config(
+    page_title='Dashboard',
+    layout='wide'
+)
+
+streamlit.title('Automation Test Dashboard XX')
+streamlit.success('Docker compose up')
+
+streamlit.write('Hello world')
