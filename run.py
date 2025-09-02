5a87ac27174e0a4b719671111a954ab53750a257
taurus5650 <taurus_5650@hotmail.com>
2025-09-02 23:42:20 +0800

    Feat: schema

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.set_page_config(
     page_title='Dashboard',
     layout='wide'
-)
+) # Browser tab name
 
 streamlit.title('Automation Test Dashboard XX')
 streamlit.success('Docker compose up')
