95254a75b19f77bc7adb3e99d1d60c4193392939
taurus5650 <taurus_5650@hotmail.com>
2025-09-05 00:54:29 +0800

    failure_insights

--- business.py
+++ business.py
@@ -14,8 +14,11 @@ class Business:
 
     def get_failure_summary_grouped_by_service(self, start_date: str, end_date: str) -> pandas.DataFrame:
         sql = ("""
-            SELECT service_team, COUNT(*) AS fail_count
-            FROM automation_test_result, jsonb_array_elements(failure_records) AS failure_records
+            SELECT
+                service_team,
+                COUNT(*) AS fail_count
+            FROM automation_test_result,
+                jsonb_array_elements(failure_records) AS failure_record
             WHERE create_time BETWEEN %s AND %s
             GROUP BY service_team
             ORDER BY fail_count DESC;
@@ -24,22 +27,21 @@ class Business:
         result = self.db.execute(sql=sql, params=(start_date, end_date), fetchall=True)
         return pandas.DataFrame(result, columns=['service_team', 'fail_count'])
 
-
-    # def fetch_recent_failures(self, limit=50):
-    #     query = """
-    #         SELECT
-    #             service_team,
-    #             service,
-    #             failed_case ->> 'name' AS test_name,
-    #             failed_case ->> 'error' AS error_message,
-    #             failed_case ->> 'traceback' AS traceback,
-    #             start_time AS timestamp
-    #         FROM automation_test_result,
-    #              jsonb_array_elements(failure_records) AS failed_case
-    #         ORDER BY start_time DESC
-    #         LIMIT %s;
-    #     """
-    #     with self.db.connection.cursor() as cursor:
-    #         cursor.execute(query, (limit,))
-    #         rows = cursor.fetchall()
-    #         return pd.DataFrame(rows, columns=['service_team', 'service', 'test_name', 'error_message', 'traceback', 'timestamp'])
+    def get_failure_details_by_team(self, service_team: str, start_date: str, end_date: str) -> pandas.DataFrame:
+        sql = ("""
+            SELECT
+                service_team,
+                service,
+                failure_record ->> 'name' AS case_name,
+                failure_record ->> 'error' AS error_message,
+                failure_record ->> 'traceback' AS traceback,
+                start_time AS timestamp
+            FROM automation_test_result,
+                jsonb_array_elements(failure_records) AS failure_record
+            WHERE service_team = %s
+                AND start_time BETWEEN %s AND %s
+                AND jsonb_array_length(failure_records) > 0
+            ORDER BY start_time DESC;
+        """)
+        result = self.db.execute(sql=sql, params=(service_team, start_date, end_date), fetchall=True)
+        return pandas.DataFrame(result, columns=['service_team', 'service', 'case_name', 'error_message', 'traceback', 'timestamp'])
--- run.py
+++ run.py
@@ -8,6 +8,7 @@ class StreamLitPage:
 
     def __init__(self):
         self.business = Business()
+        streamlit.set_page_config(page_title='Dashboard', layout='wide')
         self.page_handlers = {
             "All Team Failure Case Summary": self.all_team_failure_case_summary,
             "Failure Insights": self.failure_insights
@@ -29,37 +30,64 @@ class StreamLitPage:
     def all_team_failure_case_summary(self):
 
         start_date, end_date = streamlit.columns(2)
-
         with start_date:
-            start_date = streamlit.date_input('📅 Start date', value='2025-09-01') # elf.common_value.last_seven_day
-
+            start_date = streamlit.date_input(label='Start date', value='2025-09-01')  # elf.common_value.last_seven_day
         with end_date:
-            end_date = streamlit.date_input('📅 End date', value='2025-09-07')  # self.common_value.today
+            end_date = streamlit.date_input(label='End date', value='2025-09-07')  # self.common_value.today
 
         dataframe = self.business.get_failure_summary_grouped_by_service(start_date=start_date, end_date=end_date)
 
         if dataframe.empty:
             streamlit.warning('No failure data found.')
-        else:
-            figure = plotly.bar(
-                data_frame=dataframe,
-                x='service_team',
-                y='fail_count',
-                color='service_team',
-                color_discrete_sequence=plotly.colors.cyclical.Twilight,
-                title=f'{start_date} - {end_date}',
-                text='fail_count'
-            )
-            figure.update_layout(showlegend=False)
-            streamlit.plotly_chart(figure_or_data=figure, use_container_width=True)
+            return
+
+        figure = plotly.bar(
+            data_frame=dataframe,
+            x='service_team',
+            y='fail_count',
+            color='service_team',
+            color_discrete_sequence=plotly.colors.cyclical.Twilight,
+            title=f'{start_date} - {end_date}',
+            text='fail_count'
+        )
+        figure.update_layout(showlegend=False)
+        streamlit.plotly_chart(figure_or_data=figure, use_container_width=True)
 
     def failure_insights(self):
-        streamlit.info('Coming soon.')
+
+        all_team_dataframe = self.business.get_all_service_teams()
+        all_teams = all_team_dataframe['service_team'].tolist()
+        service_team = streamlit.selectbox(
+            label='service team',
+            options=all_teams,
+        )
+
+        start_date, end_date = streamlit.columns(2)
+        with start_date:
+            start_date = streamlit.date_input(label='Start date', value='2025-09-01')  # elf.common_value.last_seven_day
+        with end_date:
+            end_date = streamlit.date_input(label='End date', value='2025-09-03')  # self.common_value.today
+
+        dataframe = self.business.get_failure_details_by_team(
+            service_team=service_team,
+            start_date=start_date,
+            end_date=end_date
+        )
+
+        if dataframe.empty:
+            streamlit.warning('No failure data found.')
+            return
+
+        expand_all = streamlit.checkbox("Expand", value=False)
+
+        for idx, row in dataframe.iterrows():
+            with streamlit.expander(label=f'❌     {row['case_name']} | {row['timestamp']}', expanded=expand_all):
+                streamlit.error(row['error_message'])
 
     def main(self):
-        self.sidebar() # Load sidebar
-        streamlit.header(self.page) # All page will set the title
-        self.page_handlers[self.page]() # Display all pages
+        self.sidebar()  # Load sidebar
+        streamlit.header(self.page)  # All page will set the title
+        self.page_handlers[self.page]()  # Display all pages
 
 
 if __name__ == '__main__':
